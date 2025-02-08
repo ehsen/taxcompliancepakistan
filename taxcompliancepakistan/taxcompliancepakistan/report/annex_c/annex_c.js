93c7c25ba73e359bd9039478a20af4856d8186c4
Ehsen Siraj <ehsensiraj@gmail.com>
2025-02-08 15:57:11 +0500

    chore: Added fiters in Annex C, update core report logic

--- taxcompliancepakistan/taxcompliancepakistan/report/annex_c/annex_c.js
+++ taxcompliancepakistan/taxcompliancepakistan/report/annex_c/annex_c.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2025, SpotLedger and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Annex C"] = {
-	"filters": [
-
-	]
+rappe.query_reports["Annex C"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "reqd": 1
+        },
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 1
+        }
+    ]
 };
