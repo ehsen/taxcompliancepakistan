1de7fce6fb5a26d4e07643ad25449bc54c899900
Ehsen Siraj <ehsensiraj@gmail.com>
2025-05-21 13:50:09 +0500

    WIP: implemented automated WHT

---
+++ taxcompliancepakistan/taxcompliancepakistan/doctype/wht_sections/wht_sections.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, SpotLedger and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("WHT Sections", {
+// 	refresh(frm) {
+
+// 	},
+// });
