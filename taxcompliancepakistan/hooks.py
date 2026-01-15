e02b958dfe053c2d6ed7a2d5f64f747b706f8fc2
Ehsen Siraj <ehsensiraj@spotledger.net>
2026-01-15 13:54:31 +0500

    chore: moved item tax template override to taxapp

--- taxcompliancepakistan/hooks.py
+++ taxcompliancepakistan/hooks.py
@@ -135,9 +135,9 @@ doctype_js = {
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Item Tax Template": "taxcompliancepakistan.taxcompliancepakistan.overrides.custom_item_tax_template.CustomItemTaxTemplate"
+}
 
 # Document Events
 # ---------------
